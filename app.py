b335cc3b1ac7e6b69b25dc5b28e4e09c669589f1
mjouppil <markkujouppila@outlook.com>
2023-10-07 14:51:08 +0300

    Work done: - created and connected new PostgreSQL-database named "corresponder_db" and updated ".env"-file. - Modified schema.sql and created tables to db. - new "test_conn"-method to test connection to db. - new "testA.html"-template to test Bootstrap-functionalities. - new files for future dev "users.py", "routes.py", "login.html".

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
+from flask_bootstrap import Bootstrap
 from os import getenv, path
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 import secrets
 
-basedir = path.abspath(path.dirname(__file__))
-env_dir = path.join(basedir, '.env')
-load_dotenv(env_dir)
 
-print(basedir)
-print(env_dir)
-#print(getenv("SECRET_KEY"))
+def create_app():
+    basedir = path.abspath(path.dirname(__file__))
+    env_dir = path.join(basedir, '.env')
+    load_dotenv(env_dir)
 
-app = Flask(__name__)
-app.secret_key = getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+    c_app = Flask(__name__)
+    c_app.secret_key = getenv('SECRET_KEY')
+    c_app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+    c_db = SQLAlchemy(c_app)
+    c_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    Bootstrap(c_app)
 
-#print('SECRET_KEY:', app.secret_key)
-#print('DATABASE_URL:', app.config['SQLALCHEMY_DATABASE_URI'])
+    return c_app, c_db
 
-db = SQLAlchemy(app)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#session["test"] = "aybabtu"
+app, db = create_app()
 
 
 @app.route('/')
@@ -37,19 +36,23 @@ def index():
     return render_template('index.html', contacts=contacts, threads=threads)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        render_template('index.html')
+
     username = request.form['username']
     password = request.form['password']
 
-    sql = text('SELECT id, username, password, alias, visibility FROM users WHERE username=:username')
+    sql = text('SELECT id, username, password, alias, visible FROM users WHERE username=:username')
     result = db.session.execute(sql, {'username': username})
     user = result.fetchone()
-    print(user)
-    if user[2] == password:
+
+    if user and user[2] == password:
+        session['user_id'] = user[0]
         session['username'] = user[1]
         session['alias'] = user[3]
-        session['visibility'] = user[4]
+        session['visible'] = user[4]
 
     return redirect('/')
 
@@ -63,32 +66,54 @@ def logout():
 @app.route('/create_user')
 def create_user():
     return render_template('create_user.html')
+
+
 @app.route('/new_user', methods=['POST'])
 def new_user():
+
+    test_conn()
+
     print(request.form)
     username = request.form['username']
     password = request.form['password']
     alias = request.form['alias']
-    visibility = True if 'visibility' in request.form else False
+    visible = 'true' if 'visible' in request.form else 'false'
     sql = text('SELECT id, username FROM users WHERE username=:username')
     result = db.session.execute(sql, {'username': username})
     user = result.fetchone()
-    print(username, password, alias, visibility)
+    print(username, password, alias, visible)
 
     if user:
         print('USER FOUND: ', user)
         return redirect('/create_user')
     else:
-        sql = text('INSERT INTO users (username, password, alias, visibility) VALUES (:username, :password, :alias, :visibility)')
-        db.session.execute(sql, {'username': username, 'password': password, 'alias': alias, 'visibility': visibility})
+        sql = text('INSERT INTO users (username, password, alias, visible) VALUES (:username, :password, :alias, :visible)')
+        db.session.execute(sql, {'username': username, 'password': password, 'alias': alias, 'visible': visible})
         db.session.commit()
     return redirect('/')
 
 
+@app.route('/testA')
+def testA():
+    return render_template('base.html')
+
+
+def test_conn():
+    with app.app_context():
+        try:
+            # db.session.execute('SELECT 1')
+            db.session.execute(text('SELECT 1'))
+            print('\n\n----------- Connection successful !')
+        except Exception as e:
+            print('\n\n----------- Connection failed ! ERROR : ', e)
+    return
+
+
 def get_token():
-    token = secrets.token_hex(16)
+    token = secrets.token_hex(12)
     return token
 
+#print(get_token())
 
 if __name__ == '__main__':
     app.run()
--- schema.sql
+++ schema.sql
@@ -1,7 +1,40 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT,
-    password TEXT,
-    alias TEXT,
-    visibility BOOLEAN
+    username VARCHAR(48),
+    password VARCHAR(24),
+    alias VARCHAR(48),
+    visible BOOLEAN
+);
+
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    contact_id INTEGER REFERENCES users,
+    pending BOOLEAN
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(48)
+);
+
+CREATE TABLE user_threads (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads,
+    message TEXT,
+    time TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE contact_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    token VARCHAR(24),
+    active BOOLEAN
 );
